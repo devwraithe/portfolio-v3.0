8ef49e443eb6c0002033a64bb92f338aea8f7f43
Ibrahim Ibrahim <folahanmi0001@gmail.com>
2022-12-19 09:12:44 +0100

    Complete Revamp;

--- lib/src/config/theme.dart
+++ lib/src/config/theme.dart
@@ -1,56 +1,56 @@
 import 'package:flutter/material.dart';
 
 ThemeData themeData = ThemeData(
-  fontFamily: "Articulat",
+  fontFamily: "Articulat CF",
   textTheme: const TextTheme(
     headline1: TextStyle(
       fontSize: 68.80,
-      color: Colors.white,
+      color: Colors.black,
     ),
     headline2: TextStyle(
       fontSize: 57.33,
-      color: Colors.white,
+      color: Colors.black,
     ),
     headline3: TextStyle(
       fontSize: 47.78,
-      color: Colors.white,
+      color: Colors.black,
     ),
     headline4: TextStyle(
       fontSize: 39.81,
-      color: Colors.white,
+      color: Colors.black,
     ),
     headline5: TextStyle(
       fontSize: 33.18,
-      color: Colors.white,
+      color: Colors.black,
     ),
     headline6: TextStyle(
       fontSize: 27.65,
-      color: Colors.white,
+      color: Colors.black,
       fontWeight: FontWeight.w600,
     ),
     subtitle1: TextStyle(
       fontSize: 23.04,
-      color: Colors.white,
+      color: Colors.black,
     ),
     subtitle2: TextStyle(
       fontSize: 19.20,
-      color: Colors.white,
+      color: Colors.black,
     ),
     bodyText1: TextStyle(
       fontSize: 16.00,
-      color: Colors.white,
+      color: Colors.black,
     ),
     bodyText2: TextStyle(
       fontSize: 12.80,
-      color: Colors.white,
+      color: Colors.black,
     ),
     caption: TextStyle(
       fontSize: 10.24,
-      color: Colors.white,
+      color: Colors.black,
     ),
     overline: TextStyle(
       fontSize: 8.19,
-      color: Colors.white,
+      color: Colors.black,
     ),
   ),
 );
--- lib/src/presentation/views/home.dart
+++ lib/src/presentation/views/home.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
-import 'package:portfolio_v3/src/presentation/widgets/information.dart';
-import 'package:portfolio_v3/src/presentation/widgets/projects.dart';
-import 'package:portfolio_v3/src/presentation/widgets/responsive.dart';
-import 'package:portfolio_v3/src/presentation/widgets/toolkit_card.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+  const Home({super.key});
   @override
   State<Home> createState() => _HomeState();
 }
@@ -16,52 +10,199 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
+    final screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
-      backgroundColor: const Color(0XFF141414),
-      body: Responsive.isMobile(context) || Responsive.isTablet(context)
-          ? SingleChildScrollView(
-              padding: Responsive.isMobile(context)
-                  ? const EdgeInsets.symmetric(
-                      horizontal: 22,
-                      vertical: 28,
-                    )
-                  : const EdgeInsets.symmetric(
-                      horizontal: 38,
-                      vertical: 32,
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 72),
+          child: Column(
+            children: [
+              // >>> Navigation Bar <<< //
+              Padding(
+                padding: const EdgeInsets.only(top: 38),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Craftsman, Developer, Engineer",
+                          style: textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        const SizedBox(width: 42),
+                        Text(
+                          "Professional Hitman for Hire",
+                          style: textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        const SizedBox(width: 42),
+                        Text(
+                          "Reach out to me_at_devwraithe.com",
+                          style: textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
                     ),
-              child: Column(
+                    Text(
+                      "Download Resume",
+                      style: textTheme.bodyText1?.copyWith(
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 88),
+
+              // >>> Intro Headline <<< //
+              Text(
+                "Devwraithe is an engineer crafting bespoke mobile applications from the trenches of Ilorin, Nigeria. He's currently open to opportunities.",
+                textAlign: TextAlign.left,
+                style: textTheme.headline3?.copyWith(
+                  fontWeight: FontWeight.w400,
+                  height: 1.4,
+                ),
+              ),
+              const SizedBox(height: 100),
+              // >>> Projects Section <<< //
+              Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Information(),
-                  Responsive.isMobile(context) || Responsive.isTablet(context)
-                      ? Padding(
-                          padding: const EdgeInsets.only(top: 52),
-                          child: Text(
-                            "Things I've Done",
-                            style: Responsive.isMobile(context)
-                                ? textTheme.subtitle1?.copyWith(
-                                    fontWeight: FontWeight.w200,
-                                    height: 1.48,
-                                  )
-                                : textTheme.headline6?.copyWith(
-                                    fontWeight: FontWeight.w200,
-                                    height: 1.42,
-                                  ),
+                  Expanded(
+                    child: Image.network(
+                      "https://uploads-ssl.webflow.com/6211198f70481836fc440447/6218ec70a676385335367c71_astrazeneca-p-1080.jpeg",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(width: 54),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Samsung Pay",
+                          style: textTheme.subtitle2?.copyWith(
+                            fontWeight: FontWeight.w400,
                           ),
-                        )
-                      : const SizedBox(),
-                  const SizedBox(height: 32),
-                  const Projects(),
+                        ),
+                        const SizedBox(height: 6),
+                        Text(
+                          "Determination and Control",
+                          style: textTheme.headline5?.copyWith(
+                            fontWeight: FontWeight.w400,
+                            height: 1.2,
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        Text(
+                          "BrustKrebs is an interactive breast cancer information platform where you can see the different stages and versions of cancer. On the platform you can find more information about the subject and the right treatment. Samsung Pay is designed exclusively for peers to transfer money fast and easy.",
+                          style: textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w400,
+                            height: 1.52,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-            )
-          : Row(
-              children: const [
-                Information(),
-                Projects(),
-              ],
-            ),
+              const SizedBox(height: 32),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Image.network(
+                      "https://uploads-ssl.webflow.com/6211198f70481836fc440447/6218ec70a676385335367c71_astrazeneca-p-1080.jpeg",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(width: 54),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Samsung Pay",
+                          style: textTheme.subtitle2?.copyWith(
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        const SizedBox(height: 6),
+                        Text(
+                          "Determination and Control",
+                          style: textTheme.headline5?.copyWith(
+                            fontWeight: FontWeight.w400,
+                            height: 1.2,
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        Text(
+                          "BrustKrebs is an interactive breast cancer information platform where you can see the different stages and versions of cancer. On the platform you can find more information about the subject and the right treatment. Samsung Pay is designed exclusively for peers to transfer money fast and easy.",
+                          style: textTheme.bodyText1?.copyWith(
+                            fontWeight: FontWeight.w400,
+                            height: 1.52,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 140),
+
+              // >>> Contact Section <<< //
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Send me an email at_devwraithe.com",
+                    style: textTheme.headline4,
+                  ),
+                  const SizedBox(height: 22),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "GitHub",
+                            style: textTheme.subtitle1,
+                          ),
+                          const SizedBox(width: 34),
+                          Text(
+                            "LinkedIn",
+                            style: textTheme.subtitle1,
+                          ),
+                          const SizedBox(width: 34),
+                          Text(
+                            "Twitter",
+                            style: textTheme.subtitle1,
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "Designed and built by Ibrahim Ibrahim",
+                        style: textTheme.subtitle1,
+                      ),
+                      Text(
+                        "©2022",
+                        style: textTheme.subtitle1,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 38),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
